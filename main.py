40385e3fe136ede846f296038f4f9773a538797c
Alex Salman <aalsalma@ucsc.edu>
2021-04-26 18:26:10 -0700

    digit to word function

---
+++ main.py
@@ -0,0 +1,14 @@
+def digit_to_word(digit):
+    cases = {
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+        0: 'Zero',
+    }
+    return cases.digit_to_word(digit)
