e7b5e9fb057e1b99c9a760f372028725a5f27657
Alex Salman <aalsalma@ucsc.edu>
2021-04-26 18:43:33 -0700

    working with incorrect input and output  methods

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+string_list = []
+stack = []
 def digit_to_word(digit):
     cases = {
         1: 'One',
@@ -11,4 +13,28 @@ def digit_to_word(digit):
         9: 'Nine',
         0: 'Zero',
     }
-    return cases.digit_to_word(digit)
+    return cases.get(digit)
+
+def int_to_string(ls):
+    for i in ls:
+        if i > 9:
+            while i > 9:
+                x = i % 10
+                print('x',x)
+                stack.append(digit_to_word(x))
+                i = i // 10
+                print('i',i)
+            a = digit_to_word(i)
+            while (len(stack)>0):
+                a += stack.pop()
+            string_list.append(a)
+        else:
+            string_list.append(digit_to_word(i))
+    print(string_list)
+
+def main():
+    int_to_string([3, 25, 209])
+#3 25 209
+#10 300 5
+if __name__ == "__main__":
+    main()
